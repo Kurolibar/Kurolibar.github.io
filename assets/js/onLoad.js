be82ea19ed75202edd5c813fadbecd9039d686ce
Kurolibar <zaffarock@gmail.com>
2024-11-06 21:35:41 +1100

    renamed:    assets/fonts/Nunito/Nunito-Italic-VariableFont_wght.ttf -> assets/fonts/Nunito/Nunito-Italic.ttf 	renamed:    assets/fonts/Nunito/Nunito-VariableFont_wght.ttf -> assets/fonts/Nunito/Nunito.ttf 	new file:   assets/js/onLoad.js 	new file:   assets/styles/base.css 	modified:   index.html

---
+++ assets/js/onLoad.js
@@ -0,0 +1,8 @@
+const stylesheet = document.getElementById("base");
+stylesheet.onload = () => {
+  // Idk
+};
+
+stylesheet.onerror = () => {
+  console.log("An error occurred loading the stylesheet!");
+};
